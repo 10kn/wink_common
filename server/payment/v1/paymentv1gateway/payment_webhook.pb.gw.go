7cfcba3ae053d92b165772371c0fd74194f43b11
quanba3008 <113974246+quanba3008@users.noreply.github.com>
2025-02-14 14:56:18 +0700

    Fixbug webhook: NewClient function not found (#3)

--- server/payment/v1/paymentv1gateway/payment_webhook.pb.gw.go
+++ server/payment/v1/paymentv1gateway/payment_webhook.pb.gw.go
@@ -10,7 +10,6 @@ package paymentv1
 
 import (
 	"context"
-	"errors"
 	"io"
 	"net/http"
 
@@ -26,53 +25,62 @@ import (
 )
 
 // Suppress "imported and not used" errors
-var (
-	_ codes.Code
-	_ io.Reader
-	_ status.Status
-	_ = errors.New
-	_ = runtime.String
-	_ = utilities.NewDoubleArray
-	_ = metadata.Join
-)
+var _ codes.Code
+var _ io.Reader
+var _ status.Status
+var _ = runtime.String
+var _ = utilities.NewDoubleArray
+var _ = metadata.Join
 
 func request_PaymentWebhookService_Webhook_0(ctx context.Context, marshaler runtime.Marshaler, client extPaymentv1.PaymentWebhookServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var (
-		protoReq extPaymentv1.PaymentWebhookRequest
-		metadata runtime.ServerMetadata
-	)
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq.Payload); err != nil && !errors.Is(err, io.EOF) {
+	var protoReq extPaymentv1.PaymentWebhookRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Payload); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
+
 	msg, err := client.Webhook(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
+
 }
 
 func local_request_PaymentWebhookService_Webhook_0(ctx context.Context, marshaler runtime.Marshaler, server extPaymentv1.PaymentWebhookServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var (
-		protoReq extPaymentv1.PaymentWebhookRequest
-		metadata runtime.ServerMetadata
-	)
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq.Payload); err != nil && !errors.Is(err, io.EOF) {
+	var protoReq extPaymentv1.PaymentWebhookRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Payload); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
+
 	msg, err := server.Webhook(ctx, &protoReq)
 	return msg, metadata, err
+
 }
 
 // RegisterPaymentWebhookServiceHandlerServer registers the http handlers for service PaymentWebhookService to "mux".
 // UnaryRPC     :call PaymentWebhookServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterPaymentWebhookServiceHandlerFromEndpoint instead.
-// GRPC interceptors will not work for this type of registration. To use interceptors, you must use the "runtime.WithMiddlewares" option in the "runtime.NewServeMux" call.
 func RegisterPaymentWebhookServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server extPaymentv1.PaymentWebhookServiceServer) error {
-	mux.Handle(http.MethodPost, pattern_PaymentWebhookService_Webhook_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+
+	mux.Handle("POST", pattern_PaymentWebhookService_Webhook_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/payment.v1.PaymentWebhookService/Webhook", runtime.WithHTTPPathPattern("/api/v1/payment/webhook"))
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/payment.v1.PaymentWebhookService/Webhook", runtime.WithHTTPPathPattern("/api/v1/payment/webhook"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -84,7 +92,9 @@ func RegisterPaymentWebhookServiceHandlerServer(ctx context.Context, mux *runtim
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
+
 		forward_PaymentWebhookService_Webhook_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
 	})
 
 	return nil
@@ -93,24 +103,25 @@ func RegisterPaymentWebhookServiceHandlerServer(ctx context.Context, mux *runtim
 // RegisterPaymentWebhookServiceHandlerFromEndpoint is same as RegisterPaymentWebhookServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterPaymentWebhookServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.NewClient(endpoint, opts...)
+	conn, err := grpc.DialContext(ctx, endpoint, opts...)
 	if err != nil {
 		return err
 	}
 	defer func() {
 		if err != nil {
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 			return
 		}
 		go func() {
 			<-ctx.Done()
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 		}()
 	}()
+
 	return RegisterPaymentWebhookServiceHandler(ctx, mux, conn)
 }
 
@@ -124,13 +135,16 @@ func RegisterPaymentWebhookServiceHandler(ctx context.Context, mux *runtime.Serv
 // to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "extPaymentv1.PaymentWebhookServiceClient".
 // Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "extPaymentv1.PaymentWebhookServiceClient"
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
-// "extPaymentv1.PaymentWebhookServiceClient" to call the correct interceptors. This client ignores the HTTP middlewares.
+// "extPaymentv1.PaymentWebhookServiceClient" to call the correct interceptors.
 func RegisterPaymentWebhookServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client extPaymentv1.PaymentWebhookServiceClient) error {
-	mux.Handle(http.MethodPost, pattern_PaymentWebhookService_Webhook_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+
+	mux.Handle("POST", pattern_PaymentWebhookService_Webhook_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/payment.v1.PaymentWebhookService/Webhook", runtime.WithHTTPPathPattern("/api/v1/payment/webhook"))
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/payment.v1.PaymentWebhookService/Webhook", runtime.WithHTTPPathPattern("/api/v1/payment/webhook"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -141,8 +155,11 @@ func RegisterPaymentWebhookServiceHandlerClient(ctx context.Context, mux *runtim
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
+
 		forward_PaymentWebhookService_Webhook_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
 	})
+
 	return nil
 }
 
